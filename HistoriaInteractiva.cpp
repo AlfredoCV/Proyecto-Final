0581a8059cbc73f39c3d1014251faab1c3a61f36
AlfredoCV <luisalfredocastro2000@gmail.com>
2023-11-21 21:56:41 -0600

    Redacción de la introduccion de la historia

--- HistoriaInteractiva.cpp
+++ HistoriaInteractiva.cpp
@@ -10,5 +10,15 @@ int main(){
 	cout << "***********BIENVENIDO AL APOCALIPSIS ZOMBIE***********" << endl;
 	cout << "Ingresa un nombre de usuario (Maximo 15 caracteres)" << endl;
 	cin.getline(nombre, 15, '\n');
+	cout << endl;
+	
+	
+	cout << "\t\t\t\t\t\t" << vida << " HP" << endl;
+	cout << "*Llamada entrante*...\n Hola " << nombre << " me alegro de que por fin contestaste, ¿Como estas? Es una completa pesadilla lo que esta pasando afuera. ";
+	cout << "La gente esta enloqueciendo, son muy violentos y nadie sabe que es lo que pasa, el gobierno solo ha declarado estado de emergencia y toque de queda, ";
+	cout << "pero la gente lo esta ignorando. Yo tratare de llegar a un lugar seguro y esperare a que todo pase, tu trata de hacer lo mismo." << endl;
+	cout << "Cuidate mucho amigo, ¡Suerte!" << endl;
+	
+	
 	return 0;
 }
